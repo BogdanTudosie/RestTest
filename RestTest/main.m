86e52acb1a8b1bfb0a66c48fa8180701991b4a66
Bogdan Tudosie <bogdantbsd@gmail.com>
2014-04-28 10:16:32 +0300

    Initial Commit

---
+++ RestTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestTest
+//
+//  Created by Bogdan Tudosie on 28/04/14.
+//  Copyright (c) 2014 Bogdan Tudosie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BTAppDelegate class]));
+    }
+}
